e7fc238938e9e10f770b6fe103df9dbf396854b0
mohammedAK1991 <mohmed.ak.17@gmail.com>
2023-11-11 18:11:50 +0000

    feat(MongooseModule): connect mongooseModel to ninja module

--- src/ninjas/ninjas.module.ts
+++ src/ninjas/ninjas.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { NinjasController } from './ninjas.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { NinjaModel, NinjaSchema } from './ninja.schema';
 import { NinjasService } from './ninjas.service';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: NinjaModel.name, schema: NinjaSchema }]),
+    // Add any other modules you are using in NinjasModule
+  ],
   controllers: [NinjasController],
   providers: [NinjasService],
 })
