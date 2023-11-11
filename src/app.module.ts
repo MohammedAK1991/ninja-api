b4eabf7010227e800e80128585c0dc9b446975e9
mohammedAK1991 <mohmed.ak.17@gmail.com>
2023-11-11 17:50:35 +0000

    chore(connect): connect app to mongoDB atlas cluster

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NinjasModule } from './ninjas/ninjas.module';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
+const uri =
+  'mongodb+srv://mohammedak1991:Aydinzaki_123@cluster0.k5gzuww.mongodb.net/?retryWrites=true&w=majority';
 
 @Module({
-  imports: [NinjasModule, UsersModule],
+  imports: [NinjasModule, UsersModule, MongooseModule.forRoot(uri)],
   controllers: [AppController],
   providers: [AppService],
 })
