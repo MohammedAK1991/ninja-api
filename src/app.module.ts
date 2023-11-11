db2b58a47c21320b837bba1b920af63625146aa9
mohammedAK1991 <mohmed.ak.17@gmail.com>
2023-11-11 18:10:42 +0000

    feat(dotenv): move mongoDB credentials to .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,15 @@ import { NinjasModule } from './ninjas/ninjas.module';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
-const uri =
-  'mongodb+srv://mohammedak1991:Aydinzaki_123@cluster0.k5gzuww.mongodb.net/?retryWrites=true&w=majority';
-
 @Module({
-  imports: [NinjasModule, UsersModule, MongooseModule.forRoot(uri)],
+  imports: [
+    NinjasModule,
+    UsersModule,
+    MongooseModule.forRoot(
+      process.env.MONGODB_URI ||
+        'mongodb+srv://mohammedak1991:Aydinzaki_123@cluster0.k5gzuww.mongodb.net/?retryWrites=true&w=majority',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
