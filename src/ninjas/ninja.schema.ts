78c00b9e8831e7cfc1147eacc12a4f0b80c39251
mohammedAK1991 <mohmed.ak.17@gmail.com>
2023-11-12 12:23:17 +0000

    chore: add .env.production

--- src/ninjas/ninja.schema.ts
+++ src/ninjas/ninja.schema.ts
@@ -3,8 +3,18 @@
 import * as mongoose from 'mongoose';
 
 export const NinjaSchema = new mongoose.Schema({
-  name: String,
-  weapon: String,
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 100,
+  },
+  weapon: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 100,
+  },
   // other fields...
 });
 
